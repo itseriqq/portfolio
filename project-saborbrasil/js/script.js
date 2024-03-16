58b43c09b36a83d37f80a8faf2ea6c5ef1f53211
Erick <erickaug13@hotmail.com>
2024-03-16 17:28:06 -0300

    Uploading all project of SaborBrasil done

--- project-saborbrasil/js/script.js
+++ project-saborbrasil/js/script.js
@@ -15,3 +15,21 @@ var swiper = new Swiper(".mySwiper", {
       disableOnInteraction: false,
     },
   });
+
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });   
